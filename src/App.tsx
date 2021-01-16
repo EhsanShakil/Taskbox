8d8fd9f59bba2dd2779fe7203ebbf25dc09061e0
EhsanShakil <ahsanshakeel1994@gmail.com>
2021-01-16 23:53:54 +0500

    Updaitng

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
-import InboxScreen from "./Components/InboxScreen/InboxScreen";
+import Taskbox from "./Components/InboxScreen/InboxScreen";
 
 function App() {
   return (
     <div className="App">
-      <h1>Task Box</h1>
-      <span>{<InboxScreen />}</span>
-    </div>
+      <Taskbox />
+      </div>
   );
 }
 
---
+++ src/Components/Taskbox/Taskbox.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import InboxScreen from "../InboxScreen/InboxScreen";
+
+function Taskbox() {
+  return (
+    <div>
+      <h1>Task Box</h1>
+      <span>{<InboxScreen />}</span>
+    </div>
+  );
+}
+
+export default Taskbox;
